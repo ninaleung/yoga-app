3faec1b8e8b7c2e573312805a02fc5d4be81d9f1
Nina Leung <nleung320@gmail.com>
2015-12-16 20:56:24 -0600

    new yoga class form

--- app/controllers/yoga_classes_controller.rb
+++ app/controllers/yoga_classes_controller.rb
@@ -8,17 +8,22 @@ class YogaClassesController < ApplicationController
   end
 
   def new
+    # if current user is admin or studio owner
   end
 
   def create
+    # if current user is admin or studio owner
   end
 
   def edit
+    # if current user is admin or studio owner or teacher
   end
 
   def update
+    # if current user is admin or studio owner or teacher
   end
 
   def destroy
+    # if current user is admin or studio owner
   end
 end
