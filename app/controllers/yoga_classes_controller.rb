48e970d4dc1e2cdc9463d0e2a94210ebd0d7f2e5
Nina Leung <nleung320@gmail.com>
2015-12-16 21:14:34 -0600

    controller code for update method

--- app/controllers/yoga_classes_controller.rb
+++ app/controllers/yoga_classes_controller.rb
@@ -33,6 +33,18 @@ class YogaClassesController < ApplicationController
 
   def update
     # if current user is admin or studio owner or teacher
+    yoga_class = YogaClass.find_by(id: params[:id])
+    yoga_class.update(
+      name: params[:name],
+      studio_id: params[:studio_id],
+      date: params[:date],
+      time: params[:time],
+      duration: params[:duration],
+      price: params[:price],
+      level: params[:level],
+      user_id: params[:user_id]
+      )
+    redirect_to "/yoga_classes/#{yoga_class.id}"
   end
 
   def destroy
