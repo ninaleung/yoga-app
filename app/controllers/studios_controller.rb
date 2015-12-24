f8931b4a68cc5fd5bce8161505c1246b3f48b62a
Nina Leung <nleung320@gmail.com>
2015-12-23 20:54:36 -0600

    only admins have access to studios views

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,4 +1,6 @@
 class StudiosController < ApplicationController
+before_action :authenticate_admin!
+
   def index
     @studios = Studio.all
   end
@@ -8,7 +10,6 @@ class StudiosController < ApplicationController
   end
 
   def new
-
   end
 
   def create
@@ -21,5 +22,13 @@ class StudiosController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+  def authenticate_admin!
+    unless user_signed_in? && current_user.role_id == 1
+      flash[:alert] = "Sorry, you must be an admin to access studios!"
+      redirect_to "/"
+    end
   end
 end
