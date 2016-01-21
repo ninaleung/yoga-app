3c4619cee16930e14215f6f6258a71b878c88c60
Nina Leung <nleung320@gmail.com>
2016-01-21 17:19:19 -0600

    removed unirest things

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -10,9 +10,6 @@ class StudiosController < ApplicationController
       @studios = Studio.all.order(:name)
     end
 
-    # @chicago_studios = ChicagoYogaStudios::Business.all
-    # @yoga_studios = Unirest.get("https://data.cityofchicago.org/resource/uupf-x98q.json?$q=yoga").body
-    # @current = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22chicago%2C%20il%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body["query"]["results"]["channel"]["item"]
   end
 
   def show
